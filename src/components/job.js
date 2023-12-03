006b0a58b26ca066cfb088888dde9b2587d4998f
NicoleMurillo <n.murillof@uniandes.edu.co>
2023-12-03 14:49:00 -0500

    cambios

--- src/components/job.js
+++ src/components/job.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { FormattedDate, FormattedMessage } from 'react-intl';
 
 const Job = (props) => {
   return (
     <tr>
       <th scope="row">{props.offer.id}</th>
-      <td>{props.offer.name}</td>
-      <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
-      <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
+      <td><FormattedMessage id="Position" /></td>
+      <td><FormattedMessage id="Company" /></td>
+      <td>{props.offer.salary.toLocaleString()} <FormattedMessage id="million" /></td>
+      <td><FormattedMessage id="City" values={{ city: props.offer.city }} /></td>
+      <td>
+        <FormattedDate
+          value={new Date(props.offer.date)}
+          year='numeric'
+          month='long'
+          day='numeric'
+          weekday='long'
+        />
+      </td>
+      <th scope="col"><FormattedMessage id="Views" /></th>
     </tr>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {IntlProvider} from 'react-intl';
+import { IntlProvider } from 'react-intl';
+import JobsList from "./components/jobslist";
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
+import "./styles.css";
 
-import JobsList from "./components/jobsList";
+const language = navigator.language || navigator.userLanguage;
+const isSpanish = language.toLowerCase().includes('es');
+
+const headerBackgroundColor = isSpanish ? 'light' : 'dark';
 
 ReactDOM.render(
-        <IntlProvider locale="en">
-                <JobsList/>
-        </IntlProvider>, document.getElementById("root")
+  <IntlProvider locale={isSpanish ? "es-ES" : "en-US"} messages={isSpanish ? localeEsMessages : localeEnMessages}>
+    <JobsList headerBackgroundColor={headerBackgroundColor} />
+  </IntlProvider>, document.getElementById("root")
 );
